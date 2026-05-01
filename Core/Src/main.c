21683e255e73410cb288b1020e7d2356fedb090d
Facundo Mourelle <facundo.mourelle@mi.unc.edu.ar>
2026-05-01 02:07:27 -0300

    fix: include

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,11 +20,9 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "gpio.h"
-#include "semphr.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "semphr.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
